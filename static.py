d0a5b3e6861f1e376c927699941023872fba4fa1
insilications <boboniboni@gmail.com>
2020-08-06 05:09:16 -0300

    Small fixes & housekeeping.

---
+++ static.py
@@ -0,0 +1,39 @@
+#!/bin/true
+#
+
+import configparser
+import os
+import re
+import subprocess
+import sys
+import textwrap
+import hashlib
+import shlex
+import sys
+from collections import OrderedDict
+
+def open_auto(*args, **kwargs):
+    """Open a file with UTF-8 encoding.
+
+    Open file with UTF-8 encoding and "surrogate" escape characters that are
+    not valid UTF-8 to avoid data corruption.
+    """
+    # 'encoding' and 'errors' are fourth and fifth positional arguments, so
+    # restrict the args tuple to (file, mode, buffering) at most
+    assert len(args) <= 3
+    assert 'encoding' not in kwargs
+    assert 'errors' not in kwargs
+    return open(*args, encoding="utf-8", errors="surrogateescape", **kwargs)
+
+def main():
+    libs_file = "/insilications/cfg/test4"
+    libs_re = r"(extralibs_)(|avutil|avcodec|avformat|avdevice|avfilter|avresample|postproc|swscale|swresample)(?==)"
+    if os.path.exists(libs_file):
+        with open_auto(libs_file, 'r') as libs:
+                libs_lines = libs.readlines()
+                # print("{} \n".format(libs_lines))
+                for line in libs_lines:
+                    # print("{} \n".format(line))
+                    print("{} \n".format(re.search(libs_re, line).group(0)))
+if __name__ == '__main__':
+    main()
