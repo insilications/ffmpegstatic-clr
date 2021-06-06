849ff154fb1f1b1487220509e4c5da1f12650397
Francisco Boni <boboniboni@gmail.com>
2021-06-06 11:26:56 -0300

    Small fixes & housekeeping.

--- static.py
+++ static.py
@@ -90,39 +90,35 @@ def main():
                         # for lib_matched_file in lib_matched_files:
                         #     print("{0}".format(lib_matched_file))
                         for f in os.scandir("/usr/local/cuda/lib64"):
-                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a":
-                                if lib_matched_path_re.match(f.name):
-                                    libs_dict[ff_lib].append(f.path)
-                                    breakIt = True
-                                    break
-                                    # print("{0}".format(f.path))
+                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a" and lib_matched_path_re.match(f.name):
+                                libs_dict[ff_lib].append(f.path)
+                                breakIt = True
+                                break
+                                # print("{0}".format(f.path))
                         if breakIt is True:
                             continue
                         for f in os.scandir("/usr/lib64"):
-                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a":
-                                if lib_matched_path_re.match(f.name):
-                                    libs_dict[ff_lib].append(f.path)
-                                    breakIt = True
-                                    break
-                                    # print("{0}".format(f.path))
+                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a" and lib_matched_path_re.match(f.name):
+                                libs_dict[ff_lib].append(f.path)
+                                breakIt = True
+                                break
+                                # print("{0}".format(f.path))
                         if breakIt is True:
                             continue
                         for f in os.scandir("/usr/lib"):
-                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a":
-                                if lib_matched_path_re.match(f.name):
-                                    libs_dict[ff_lib].append(f.path)
-                                    breakIt = True
-                                    break
-                                    # print("{0}".format(f.path))
+                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a" and lib_matched_path_re.match(f.name):
+                                libs_dict[ff_lib].append(f.path)
+                                breakIt = True
+                                break
+                                # print("{0}".format(f.path))
                         if breakIt is True:
                             continue
                         for f in os.scandir("/usr/nvidia/lib64"):
-                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a":
-                                if lib_matched_path_re.match(f.name):
-                                    libs_dict[ff_lib].append(f.path)
-                                    breakIt = True
-                                    break
-                                    # print("{0}".format(f.path))
+                            if f.is_file() and os.path.splitext(f.name)[1].lower() in ".a" and lib_matched_path_re.match(f.name):
+                                libs_dict[ff_lib].append(f.path)
+                                breakIt = True
+                                break
+                                # print("{0}".format(f.path))
                         if breakIt is True:
                             continue
                         # for dirpath, dirnames, filenames in os.walk("/usr/cuda/lib64", followlinks=True):
